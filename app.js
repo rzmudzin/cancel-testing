5f476100162651705e6e7c0860209a7f9a0b99a6
robz <rob@rob.com>
2023-05-01 08:47:04 -0700

    Experimenting

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
-// async function run() {
-//     // This should be a token with access to your repository scoped in as a secret.
-//     // The YML workflow will need to set myToken with the GitHub Secret Token
-//     // myToken: ${{ secrets.GITHUB_TOKEN }}
-//     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-//     const myToken = core.getInput('myToken');
 
-//     const octokit = github.getOctokit(myToken)
-
-//     // You can also pass in additional options as a second parameter to getOctokit
-//     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-//     const { data: pullRequest } = await octokit.rest.pulls.get({
-//         owner: 'octokit',
-//         repo: 'rest.js',
-//         pull_number: 123,
-//         mediaType: {
-//           format: 'diff'
-//         }
-//     });
-
-//     console.log(pullRequest);
-// }
-
-// run();
 async function run() {
     console.log('Hello Demo');
     process.argv.forEach(function (val, index, array) {
         console.log(index + ': ' + val);
     });
-    //const token = "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";
+    
     const token = process.argv[2];
     console.log('Token: ' + token);
 
@@ -39,29 +15,43 @@ async function run() {
     const { GITHUB_RUN_ID } = process.env;
     let branch = ref.slice(11);
     let runId = Number(GITHUB_RUN_ID);
+    const workflow_id = core.getInput('workflow_id', { required: false });
     console.log('Owner: ' + owner);
     console.log('Repo" ' + repo);
 
-    // let runId = 0;
     const octokit = github.getOctokit(token);
     const { data: current_run } = await octokit.rest.actions.getWorkflowRun({
         owner,
         repo,
         run_id: Number(GITHUB_RUN_ID),
     });
-    console.log(`Cancel current run ${runId}`);
-    const result = await octokit.rest.actions.cancelWorkflowRun({
-            owner,
-            repo,
-            run_id: runId,
-        });
-    console.log(`Cancel run ${runId} responded with status ${result.status}`);
-    console.log(current_run);
 
-    var moment = require('moment');
-    var date = moment().format('LL');
-    console.log(date);
-    console.log('Done');
+    await Promise.all(workflow_ids.map(async (workflow_id) => {
+        try {
+            const { data: { total_count, workflow_runs }, } = await octokit.rest.actions.listWorkflowRuns({
+                per_page: 100,
+                owner,
+                repo,
+                workflow_id,
+                branch,
+            });
+            console.log('Run Count: ' + total_count);
+        }
+        catch (e) {
+            const msg = e.message || e;
+            console.log(`Error while canceling workflow_id ${workflow_id}: ${msg}`);
+        }
+    }));
+
+    // console.log(`Cancel current run ${runId}`);
+    // const result = await octokit.rest.actions.cancelWorkflowRun({
+    //         owner,
+    //         repo,
+    //         run_id: runId,
+    //     });
+    // console.log(`Cancel run ${runId} responded with status ${result.status}`);
+    // console.log(current_run);
+
 }
 
 run();
