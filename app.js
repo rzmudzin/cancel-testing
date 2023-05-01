c3e0a23cb9c8692fc6b1a7225d362480100c4339
robz <rob@rob.com>
2023-04-30 17:31:22 -0700

    Towars adding support for command line args

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log('Hello Demo');
-
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+  });
 var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
