197733e2ce60dec3b83be7e7d544e67b96ec6a49
robz <rob@rob.com>
2023-05-01 09:29:31 -0700

    Experimenting

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
 async function run() {
     
@@ -21,8 +24,10 @@ async function run() {
         run_id: runId,
     });
     //Log the current run for any debugging needs that arise
-    console.log(current_run);
+    // console.log(current_run);
     let workflow_id = current_run.workflow_id;
+    let status = current_run.status;
+    console.log('Run id ' + runId + ' from workflow ' + workflow_id + ' has a status of ' + status);
 
     //Extract a list of all the recent runs
     const { data: { total_count, workflow_runs }, } = await octokit.rest.actions.listWorkflowRuns({
@@ -33,6 +38,18 @@ async function run() {
         branch,
     });
     console.log('Run Count: ' + total_count);
+
+    //Poll for 1 minute
+    for(var i=0; i<12; ++i) {
+        console.log(i);
+        await delay(5000);
+        const { data: runInfo } = await octokit.rest.actions.getWorkflowRun({
+            owner,
+            repo,
+            run_id: runId,
+        });
+        console.log('Run id ' + runId + ' from workflow ' + workflow_id + ' has a status of ' + runInfo.status);
+    }
 
     //Cancel the current run
     // console.log(`Cancel current run ${runId}`);
